d3c588aab4e7624f4977b2c29ca671ba5f6963d0
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-16 15:17:34 -0300

    Add DTO's

--- 1-FilmesAPI/FilmesAPI/FilmesAPI/obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
+++ 1-FilmesAPI/FilmesAPI/FilmesAPI/obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39464a13fd663bf9a5da27722fa706e88bc3c8ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd2c9e2a6fcef3f6ac42a9ada263e837e7dcb6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
