d91c59ad8a9bffa086cf43bec6017c5a6df40c7d
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-18 15:51:00 -0300

    Concluding learning project

--- 1-FilmesAPI/FilmesAPI/FilmesAPI/obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
+++ 1-FilmesAPI/FilmesAPI/FilmesAPI/obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1baac72b51547ddc7d4f0e8c6c54c132e901103b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cf3828fc0bc67fd6bd5c0986369ae034ef90f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
