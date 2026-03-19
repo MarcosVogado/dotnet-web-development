2194d93d07a72982a4b1c07bcd0db00483ffb918
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-19 15:49:48 -0300

    Improving project, add Endereco Model and Controlller

--- 1-FilmesAPI/FilmesAPI/FilmesAPI/obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
+++ 1-FilmesAPI/FilmesAPI/FilmesAPI/obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93eab98de4402a04c08466ca5ba131853e49f4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901d822b3f5b1434759f0699a21d8908c22781d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
